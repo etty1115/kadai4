aa68bc68f900a273cb91a3f475008fb4424089f4
etty1115 <miyaty1115@gmail.com>
2023-06-01 02:08:57 +0900

    修正

--- JS/script.js
+++ JS/script.js
@@ -3,14 +3,10 @@ document.getElementById('decision-form').addEventListener('submit', function(eve
     const emotion = document.getElementById('emotion').value;
     const reason = document.getElementById('reason').value;
     const decision = {emotion, reason, date: new Date()};
-    
+
     let decisions = JSON.parse(localStorage.getItem('decisions')) || [];
     decisions.push(decision);
-    
     localStorage.setItem('decisions', JSON.stringify(decisions));
-    
-    document.getElementById('emotion').value = '';
-    document.getElementById('reason').value = '';
 });
 
 document.getElementById('summary-button').addEventListener('click', function() {
@@ -19,11 +15,40 @@ document.getElementById('summary-button').addEventListener('click', function() {
     let summaryElement = document.getElementById('summary');
     summaryElement.innerHTML = ''; // Clear previous summary
     
-    decisions.forEach(function(decision) {
-        let date = new Date(decision.date);
+    if(decisions.length > 0) {
+        let table = document.createElement('table');
+        let thead = document.createElement('thead');
+        let tbody = document.createElement('tbody');
         
-        let decisionElement = document.createElement('p');
-        decisionElement.textContent = `Date: ${date.toLocaleDateString()} - Emotion: ${decision.emotion} - Reason: ${decision.reason}`;
-        summaryElement.appendChild(decisionElement);
-    });
+        let headerRow = document.createElement('tr');
+        ['Date', 'Emotion', 'Reason'].forEach(header => {
+            let th = document.createElement('th');
+            th.textContent = header;
+            headerRow.appendChild(th);
+        });
+        thead.appendChild(headerRow);
+        table.appendChild(thead);
+
+        decisions.forEach(function(decision) {
+            let date = new Date(decision.date);
+            
+            let decisionRow = document.createElement('tr');
+            [date.toLocaleDateString(), decision.emotion, decision.reason].forEach(text => {
+                let td = document.createElement('td');
+                td.textContent = text;
+                decisionRow.appendChild(td);
+            });
+            tbody.appendChild(decisionRow);
+        });
+
+        table.appendChild(tbody);
+        summaryElement.appendChild(table);
+    } else {
+        summaryElement.textContent = 'No decisions recorded yet.';
+    }
+});
+
+document.getElementById('delete-button').addEventListener('click', function() {
+    localStorage.removeItem('decisions');
+    document.getElementById('summary').innerHTML = '';
 });
